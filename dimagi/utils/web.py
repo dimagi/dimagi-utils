d2d444d2ff2b50a63b6856d4dd44e6175ebe32c4
Mike White <m@mwhite.info>
2013-02-07 16:17:35 -0500

    dunno if we actually currently don't fail on deprecationwarnings

--- dimagi/utils/web.py
+++ dimagi/utils/web.py
@@ -27,7 +27,7 @@ def get_secure_url_base():
 def render_to_response(req, template_name, dictionary=None, **kwargs):
     """This is a legacy function that shouldn't be changed or called anywhere new."""
 
-    raise DeprecationWarning("Use django.shortcuts.render in Django>=1.3")
+    print "Deprecated, use django.shortcuts.render in Django>=1.3"
 
     """Proxies calls to django.shortcuts.render_to_response, to avoid having
        to include the global variables in every request. This is a giant hack,

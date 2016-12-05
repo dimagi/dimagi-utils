4666dbc692ffd67da9f81da69856a4c2014fa1f8
NoahCarnahan <noah.carnahan@gmail.com>
2016-12-05 11:11:31 -0500

    Update test and add decorator test

--- dimagi/utils/tests/sage_iterator.py
+++ dimagi/utils/tests/sage_iterator.py
@@ -1,14 +1,24 @@
 from django.test import SimpleTestCase
 
-from dimagi.utils.couch.database import SafeGenerator, GeneratorAlreadyConsumedException
+from dimagi.utils.safe_iterator import SafeIterator, IteratorAlreadyConsumedException, safe_generator
 
 
-class SafeGeneratorTest(SimpleTestCase):
+class SafeIteratorTest(SimpleTestCase):
 
     def test_exception(self):
         generator = (x for x in range(3))
-        safe_generator = SafeGenerator(generator)
-        self.assertEqual(list(safe_generator), [0,1,2])
-        with self.assertRaises(GeneratorAlreadyConsumedException):
+        safe_generator = SafeIterator(generator)
+        self.assertEqual(list(safe_generator), [0, 1, 2])
+        with self.assertRaises(IteratorAlreadyConsumedException):
             for i in safe_generator:
                 pass
+
+    def test_decorator(self):
+        @safe_generator
+        def func():
+            return (x for x in range(3))
+
+        generator = func()
+        self.assertEqual(list(generator), [0, 1, 2])
+        with self.assertRaises(IteratorAlreadyConsumedException):
+            list(generator)
